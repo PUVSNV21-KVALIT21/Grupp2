31acf0b61ab5e2d502b2b8cdc84e1a68063bb659
DanielNygren <75730040+DanielNygren@users.noreply.github.com>
2022-04-27 22:00:50 +0200

    Created Custom Users Fields

--- Grupp2/Data/ApplicationDbContext.cs
+++ Grupp2/Data/ApplicationDbContext.cs
@@ -1,9 +1,28 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
 
 namespace Grupp2.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+
+    public class ApplicationUser : IdentityUser
+    {
+        [MaxLength(250)]
+        public string FirstName { get; set; }
+        [MaxLength(250)]
+        public string LastName { get; set; }
+        [MaxLength(250)]
+        public string Address1 { get; set; }
+        [MaxLength(250)]
+        public string Address2 { get; set; }
+        [MaxLength(50)]
+        public string PostCode { get; set; }
+        [MaxLength(50)]
+        public string PostArea { get; set; }
+    }
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
