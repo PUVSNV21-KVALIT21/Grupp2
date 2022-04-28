cee9c4cb15179e8eaceed8e9c612309752213d1b
DanielNygren <75730040+DanielNygren@users.noreply.github.com>
2022-04-28 18:10:05 +0200

    Added admin user and the ability to use Identity ot set restrictions for users.

---
+++ Grupp2/Data/Migrations/20220428132702_AddAdminAccount.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System.Text;
+
+#nullable disable
+
+namespace Grupp2.Data.Migrations
+{
+
+    public partial class AddAdminAccount : Migration
+    {
+
+        const string ADMIN_USER_GUID = "050b62da-a76f-4fe1-8d0b-d11fe6ce62d3";
+        const string ADMIN_ROLE_GUID = "a8356651-6c05-4245-9747-3044a83e54ff";
+
+        /// <summary> Adding Admin Users
+        /// 
+        /// </summary>
+        /// <param name="migrationBuilder"></param>
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var hasher = new PasswordHasher<ApplicationUser>();
+
+            //TODO Hide password.
+            var passwordHash = hasher.HashPassword(null, "Password123!");
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("INSERT INTO AspNetUsers(Id, UserName, NormalizedUserName, Email, EmailConfirmd, TowFactorEnabled, LockedoutEnabled, AccessFailedCount, NormalizedEmail, PasswordHash, SecurityStamp,FirstName)");
+            sb.AppendLine("VALUES(");
+            sb.AppendLine($"'{ADMIN_USER_GUID}'");
+            sb.AppendLine(",'admin@hakimlivs.se'");
+            sb.AppendLine(",'ADMIN@HAKIMLIVS.se'");
+            sb.AppendLine(",'admin@hakimlivs.se'");
+            sb.AppendLine(", 0");
+            sb.AppendLine(", 0");
+            sb.AppendLine(", 0");
+            sb.AppendLine(", 0");
+            sb.AppendLine(", 0");
+            sb.AppendLine(",'ADMIN@HAKIMLIVS.se'");
+            sb.AppendLine($", '{passwordHash}'");
+            sb.AppendLine(", ''");
+            sb.AppendLine(", 'Admin'");
+            sb.AppendLine(")");
+
+            migrationBuilder.Sql(sb.ToString());
+
+            migrationBuilder.Sql($"INSERT INTO AspNetRoles (Id, Name, NormalizedName) VALUES ('{ADMIN_ROLE_GUID}', 'Admin', 'ADMIN')");
+            
+            migrationBuilder.Sql($"INSERT INTO AspNetUserRoles (UserId, RoleId) VALUES ('{ADMIN_USER_GUID}, {ADMIN_ROLE_GUID}')");
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql($"DELETE FROM AspNetUserRoles WHERE UserId = '{ADMIN_USER_GUID}' AND RoleId = '{ADMIN_ROLE_GUID}'");
+            migrationBuilder.Sql($"DELETE FROM AspNetUsers WHERE Id = '{ADMIN_USER_GUID}");
+            migrationBuilder.Sql($"DELETE FROM AspNetUsers WHERE Id = '{ADMIN_ROLE_GUID}");
+        }
+    }
+}
--- Grupp2/Program.cs
+++ Grupp2/Program.cs
@@ -36,9 +36,16 @@ app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-app.MapRazorPages();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+      name: "areas",
+      pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+    app.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+    app.MapRazorPages();
+});
+
 
 app.Run();
