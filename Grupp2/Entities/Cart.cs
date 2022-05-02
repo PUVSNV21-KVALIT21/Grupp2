27573722a869709047597929e6342cc86e0410fc
DanielNygren <75730040+DanielNygren@users.noreply.github.com>
2022-05-02 15:27:43 +0200

    Added entites: cart, cartitem, categoty, discount, membershiplvl, product and added the relations.

---
+++ Grupp2/Entities/Cart.cs
@@ -0,0 +1,10 @@
+﻿
+
+namespace Grupp2.Entities
+{
+    public class Cart
+    {
+        public Guid Id { get; set; }
+        public List<CartItem>? CartItems { get; set; }
+    }
+}
---
+++ Grupp2/Entities/Category.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Grupp2.Entities
+{
+    public class Category
+    {
+        public Guid Id { get; set; }   
+        [Required]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "Name must be greater then or equal to 2")]
+        public string? Name { get; set; }
+    }
+}
