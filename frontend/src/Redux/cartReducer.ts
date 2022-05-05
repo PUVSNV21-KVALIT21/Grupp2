b10bf195243e98739c17b9e346cf11787b781165
Dan Strandberg <DanStrandberg_@hotmail.com>
2022-05-05 16:24:17 +0200

    added comments

--- frontend/src/Redux/cartReducer.ts
+++ frontend/src/Redux/cartReducer.ts
@@ -12,13 +12,16 @@ const cartSlice = createSlice({
       const inCart = state.shoppingCart.find((item: any) =>
         item.id === action.payload.id ? true : false
       );
+      //new code
       if (inCart) {
         const returned = Object.assign({ ...item, qty: item.qty + 1 });
         console.log(returned);
         //add qty to item
       } else {
         state.shoppingCart.push(item);
-      }
+      } // end of new code.
+
+      //nedan hanterar att istället för att lägga till en ny vara vid "köp-knappen" så läggs det till en ny kvanitet. Fick inte en förenkling att fungera, lyckades bara lägga till en ny vara
       //   return {
       //     //If item is already in cart, add + 1 qty. If not, add item to shoppingCart
       //     ...state,
@@ -29,6 +32,7 @@ const cartSlice = createSlice({
       //       : [...state.shoppingCart, { ...item, qty: 1 }],
       //   };
     },
+
     ADD_QUANTITY: (state: any, action: any) => {
       return {
         ...state,
