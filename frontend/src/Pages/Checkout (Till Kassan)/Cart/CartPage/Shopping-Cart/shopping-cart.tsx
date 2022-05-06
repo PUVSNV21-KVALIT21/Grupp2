92b5ed022fa0b8a3c0f3eb1aee5b2cebdb08cc70
Dan Strandberg <DanStrandberg_@hotmail.com>
2022-05-06 11:08:09 +0200

    Cart functionality done

--- frontend/src/Pages/Checkout (Till Kassan)/Cart/CartPage/Shopping-Cart/shopping-cart.tsx
+++ frontend/src/Pages/Checkout (Till Kassan)/Cart/CartPage/Shopping-Cart/shopping-cart.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import ItemSummary from './Item-Summary-Component/item-summary-component';
-import { connect, useDispatch } from 'react-redux';
+import { connect } from 'react-redux';
 import './shopping-cart-page-style.css';
 import {
   store,
@@ -13,15 +13,6 @@ function ShoppingCartPage({ cart }: { cart: [] }) {
   const [totalPrice, setTotalPrice] = useState(0);
 
   // useEffect(() => {}, [cart]);
-
-  const dispatch = useDispatch();
-  function DecreaseQuantity(item: any) {
-    dispatch(DECREASE_QUANTITY(item));
-    //item.qty === 0 showed 0 qty in app but required one more removal before item was removed from cart
-    if (item.qty === 1) {
-      dispatch(REMOVE_FROM_CART(item));
-    }
-  }
 
   return (
     <div className="shopping-cart-page">
@@ -37,7 +28,9 @@ function ShoppingCartPage({ cart }: { cart: [] }) {
               price={item.price}
               IncreaseQuantity={() => store.dispatch(ADD_QUANTITY(item))}
               quantity={item.qty}
-              DecreaseQuantity={() => DecreaseQuantity(item)}
+              DecreaseQuantity={() =>
+                store.dispatch(item.qty > 1 ? DECREASE_QUANTITY(item) : REMOVE_FROM_CART(item))
+              }
               RemoveFromCart={() => store.dispatch(REMOVE_FROM_CART(item))}
             />
           );
