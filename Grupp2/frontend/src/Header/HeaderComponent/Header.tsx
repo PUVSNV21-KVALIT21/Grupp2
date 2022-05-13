738da160f529236c1f39c62c4c9cbd2cff80883d
TheStrandberg <danstrandberg_@hotmail.com>
2022-05-13 16:04:20 +0200

    infinite header and footer

--- Grupp2/frontend/src/Header/HeaderComponent/Header.tsx
+++ Grupp2/frontend/src/Header/HeaderComponent/Header.tsx
@@ -10,19 +10,21 @@ function HeaderComponent() {
   const [text, setText] = useState('');
 
   return (
-    <div className="header-wrapper">
-      <Link to={'/'}>
-        <img src={logo} alt="hakims logo" className="header-logo"></img>
-      </Link>
-      <div className="header-searchbar">
-        <SearchBar query={text} onQueryChange={setText} />
-      </div>
-      <div className="header-buttons">
-        <div className="header-login-button">
-          <LogInButton />
+    <div className="infinite-div">
+      <div className="header-wrapper">
+        <Link to={'/'}>
+          <img src={logo} alt="hakims logo" className="header-logo"></img>
+        </Link>
+        <div className="header-searchbar">
+          <SearchBar query={text} onQueryChange={setText} />
         </div>
-        <div className="header-cart-button">
-          <CartButton />
+        <div className="header-buttons">
+          <div className="header-login-button">
+            <LogInButton />
+          </div>
+          <div className="header-cart-button">
+            <CartButton />
+          </div>
         </div>
       </div>
     </div>
--- Grupp2/frontend/src/Pages/Checkout (Till Kassan)/Cart/CartPage/Header/cart-header.tsx
+++ Grupp2/frontend/src/Pages/Checkout (Till Kassan)/Cart/CartPage/Header/cart-header.tsx
@@ -4,13 +4,15 @@ import './cart-header-style.css';
 
 function CartHeader() {
   return (
-    <div className="header-wrapper">
-      <Link to={'/'}>
-        <img src={logo} alt="hakims logo" className="header-logo"></img>
-      </Link>
-      <Link to="/" className="keep-shopping">
-        <span>Fortsätt shoppa</span>
-      </Link>
+    <div className="infinite-header">
+      <div className="header-wrapper">
+        <Link to={'/'}>
+          <img src={logo} alt="hakims logo" className="header-logo"></img>
+        </Link>
+        <Link to="/" className="keep-shopping">
+          <span>Fortsätt shoppa</span>
+        </Link>
+      </div>
     </div>
   );
 }
