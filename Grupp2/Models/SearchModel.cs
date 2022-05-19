110b6d8158ef2c68c5f740b14a36d6306dd2b43a
nordh95 <antonnord95@gmail.com>
2022-05-19 08:38:54 +0200

    Search-products-in-Admin-view

---
+++ Grupp2/Models/SearchModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Grupp2.Models
+{
+    public class SearchModel
+    {
+        public string? Search { get; set; }
+    }
+}
